90dbb396099460205d246477a3f9dda1989f02d6
Ayaal Kaplin <uraankhayayaal@gmail.com>
2022-04-16 11:43:25 +0900

    add sorting asset

--- src/assets/NavSortAsset.php
+++ src/assets/NavSortAsset.php
@@ -12,9 +12,11 @@ class NavSortAsset extends AssetBundle
     public $sourcePath = '@ityakutia/navigation/assets/src/';
     
     public $css = [
+        '//code.jquery.com/ui/1.13.1/themes/base/jquery-ui.css',
         'css/sortable.css',
     ];
     public $js = [
+        '//code.jquery.com/ui/1.13.1/jquery-ui.js',
         'js/init.js',
     ];
     public $depends = [
